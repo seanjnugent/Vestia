bc8f589ec8c6861a7a7d0795498b84a1f2e8f38f
seanjnugent <87038678+seanjnugent@users.noreply.github.com>
2024-11-30 13:01:00 +0000

    Update Schema.sql

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,81 +1,145 @@
-CREATE TABLE Client (
-    ClientId SERIAL PRIMARY KEY,
-    FirstName VARCHAR(100),
-    Surname VARCHAR(100),
-    DateOfBirth DATE,
-    CountryOfResidence varchar (3),
-    ResidentialAddress JSON,
-    ClientProfile JSON,
-    EmailAddress varchar(100),
-    PhoneNumber varchar(20),
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Account (
-    AccountId SERIAL PRIMARY KEY,
-    ClientId INT REFERENCES Client(ClientId),
-    AccountType VARCHAR(50),
-    AccountName VARCHAR(100),
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Asset (
-    AssetId SERIAL PRIMARY KEY,
-    AssetCode VARCHAR(50),
-    AssetName VARCHAR(100),
-    AssetType VARCHAR(50),
-    AssetStatus VARCHAR(20),
-    AssetDetails JSON,
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Instruction (
-    InstructionId SERIAL PRIMARY KEY,
-    InstructionType VARCHAR(20),
-    InstructionStatus VARCHAR(20),
-    InstructionDetails JSON,
-    Allocation JSON,
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Model (
-    ModelId SERIAL PRIMARY KEY,
-    AssetId INT REFERENCES Asset(AssetId),
-    ModelDetails JSON,
-    ModelStatus varchar (20)
-    Allocation JSON,
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Trade (
-    TradeId SERIAL PRIMARY KEY,
-    AccountId INT REFERENCES Account(AccountId),
-    ClientId INT REFERENCES Client(ClientId),
-    AssetId INT REFERENCES Asset(AssetId),
-    InstructionId INT REFERENCES Instruction(InstructionId),
-    ModelId INT REFERENCES Model(ModelId),
-    TradeQuantity NUMERIC,
-    TradeCost NUMERIC,
-    TradeType VARCHAR(50),
-    TradeStatus VARCHAR(50),
-    DatePlaced TIMESTAMP CURRENT_TIMESTAMP,
-    DateCompleted TIMESTAMP CURRENT_TIMESTAMP
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 
-CREATE TABLE Price (
-    PriceId SERIAL PRIMARY KEY,
-    AssetId INT REFERENCES Asset(AssetId),
-    CurrencyCode VARCHAR(3) DEFAULT 'USD',
-    Amount numeric
-    PriceDate TIMESTAMP,
-    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+table "client" {
+  "clientid" serial [pk, increment]
+  "firstname" varchar(100)
+  "surname" varchar(100)
+  "dateofbirth" date
+  "countryofresidence" varchar(3)
+  "residentialaddress" json
+  "clientprofile" json
+  "emailaddress" varchar(100)
+  "phonenumber" varchar(20)
+  "date_created" timestamp [default: `current_timestamp`]
+  "date_updated" timestamp [default: `current_timestamp`]
+}
+
+
+table "account" {
+  "account_id" serial [pk, increment]
+  "clientid" int
+  "accounttype" varchar(50)
+  "accountname" varchar(100)
+  "model_id"  int
+  "date_created" timestamp [default: `current_timestamp`]
+  "date_updated" timestamp [default: `current_timestamp`]
+}
+
+
+table "asset" {
+  "asset_id" serial [pk, increment]
+  "asset_code" varchar(50)
+  "asset_name" varchar(100)
+  "asset_type" varchar(50)
+  "asset_status" varchar(20)
+  "currency_code" varchar(3)
+  "asset_details" json
+  "date_created" timestamp [default: `current_timestamp`]
+  "date_updated" timestamp [default: `current_timestamp`]
+}
+
+
+table "instruction" {
+  "instruction_id" serial [pk, increment]
+  "instruction_type" varchar(20)
+  "instruction_status" varchar(20)
+  "instruction_details" json
+  "allocation" json
+  "date_created" timestamp [default: `current_timestamp`]
+  "date_updated" timestamp [default: `current_timestamp`]
+}
+
+
+table "model" {
+  "model_id" serial [pk, increment]
+  "model_details" json
+  "model_status" varchar(20)
+  "allocation" json
+  "date_created" timestamp
+  "date_updated" timestamp
+}
+
+
+table "asset_trade" {
+  "asset_trade_id" serial [pk, increment]
+  "account_id" int
+  "asset_id" int
+  "asset_trade_quantity" numeric
+  "asset_trade_unit_cost" numeric
+  "asset_trade_type" varchar(50)
+  "asset_trade_status" varchar(50)
+  "asset_trade_note" varchar(100)
+  "currency_code" varchar(3)
+  "model_id" int
+  "instruction_id" int
+  "corporate_action_id" int
+  "date_placed" timestamp
+  "date_completed" timestamp
+  "date_created" timestamp
+  "date_updated" timestamp
+}
+
+
+table "cash_trade" {
+  "cash_trade_id" serial [pk, increment]
+  "account_id" int
+  "cash_trade_amount" numeric
+  "currency_code" varchar(3)
+  "instruction_id" int
+  "corporate_action_id" int
+  "cash_trade_note" varchar(100)
+  "cash_trade_status" varchar(50)
+  "date_completed" timestamp
+  "date_created" timestamp
+  "date_updated" timestamp
+}
+
+
+table "asset_price" {
+  "price_id" serial [pk, increment]
+  "asset_id" int
+  "currency_code" varchar(3) [default: 'usd']
+  "amount" numeric
+  "price_date" timestamp
+  "date_created" timestamp
+  "date_updated" timestamp
+}
+
+
+table "corporate_action" {
+  "corporate_action_id" serial [pk, increment]
+  "asset_id" int
+  "corporate_action_type" varchar(20)
+  "ex_date" timestamp
+  "date_posted" timestamp
+  "date_created" timestamp
+  "date_updated" timestamp
+  }
+
+
+ref:"client"."clientid" < "account"."clientid"
+
+
+ref:"account"."account_id" < "asset_trade"."account_id"
+ref:"account"."account_id" < "cash_trade"."account_id"
+
+
+ref:"asset"."asset_id" < "asset_trade"."asset_id"
+
+ref:"model"."model_id" < "account"."model_id"
+
+ref:"instruction"."instruction_id" < "asset_trade"."instruction_id"
+
+
+ref:"model"."model_id" < "asset_trade"."model_id"
+
+
+ref:"asset"."asset_id" < "asset_price"."asset_id"
+
+
+ref:"cash_trade"."corporate_action_id" < "corporate_action"."corporate_action_id"
+ref:"asset_trade"."corporate_action_id" < "corporate_action"."corporate_action_id"
+ref:"asset"."asset_id" < "corporate_action"."asset_id"
+
+
+
